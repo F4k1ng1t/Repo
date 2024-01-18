cdc9a0d1e17d3dc07ea8c1a685e333b760a5dd08
F4k1ng1t <nicholj7@my.erau.edu>
2024-01-18 09:49:37 -0700

    Making Speed Adjustable
    -Speed can now be adjusted in 0.5 increments by pressing plus or minus

--- FuniCube/Assets/Player.cs
+++ FuniCube/Assets/Player.cs
@@ -13,29 +13,39 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+	float s = 0.5;
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward;
+            transform.position += Vector3.forward * s;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.back;
+            transform.position += Vector3.back * s;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.left;
+            transform.position += Vector3.left * s;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right;
+            transform.position += Vector3.right * s;
         }
         if (Input.GetKey(KeyCode.Space))
         {
-            transform.position += Vector3.up;
+            transform.position += Vector3.up * s;
         }
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            transform.position += Vector3.down;
+            transform.position += Vector3.down * s;
         }
+		if (Input.GetKey(KeyCode.P) && s <= 100)
+		{
+			s += 0.5;
+		}
+		if (Input.GetKey(KeyCode.O) && s >= 0)
+		{
+			s -= 0.5;
+		}
+		
     }
 }
